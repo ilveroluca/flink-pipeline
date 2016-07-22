49c7719d279348eb30479e81068d8095894ea26f
Luca Pireddu <pireddu@crs4.it>
2016-07-22 15:12:05 +0200

    Print list of nodes in hadoop cluster before starting

--- start_hadoop.sh
+++ start_hadoop.sh
@@ -3,6 +3,17 @@
 set -o errexit
 set -o nounset
 
+n_nodes=$(grep -c -v '^#' "${HADOOP_CONF_DIR}/slaves")
+echo "=================================================" >&2
+echo "Starting a Hadoop cluster on the following ${n_nodes} nodes:" >&2
+grep -v '^#' "${HADOOP_CONF_DIR}/slaves"
+echo "=================================================" >&2
+
+# Give the user a chance to kill the script
+echo "Hit ^C now if you changed your mind" >&2
+sleep 5
+
+
 #mkdir /data/data0{1,2}/tmp
 
 # clean logs
